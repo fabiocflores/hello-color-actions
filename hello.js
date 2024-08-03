b7870f8edeca4ee5dae4bfc735d6dcf8117cee2a
“fabiocflores” <fabiocflores@gmail.com>
2024-08-03 16:49:20 -0400

    updated hello workd

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from a hot summer in New York City!";
 console.log(hello());
 exports.hello = hello;
 
